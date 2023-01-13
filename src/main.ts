09b9e729c38c1d0127833d10bd39b4074ab129a8
milan9111 <milan9111@ukr.net>
2023-01-13 23:56:07 +0200

    uncommented swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-// import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 5000;
@@ -8,14 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('dApp Betting')
-  //   .setDescription('Documentation REST API')
-  //   .setVersion('0.0.1')
-  //   .build()
+  const config = new DocumentBuilder()
+    .setTitle('dApp Betting')
+    .setDescription('Documentation REST API')
+    .setVersion('0.0.1')
+    .build()
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('/api/docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
