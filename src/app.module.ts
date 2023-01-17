39f54e54b538e935d844584fa09a3f6f0f2982cd
milan9111 <milan9111@ukr.net>
2023-01-17 13:47:15 +0200

    added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { EventsModule } from './events/events.module';
-
+import { ConfigModule } from '@nestjs/config';
 import { MatchesModule } from './matches/matches.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     MatchesModule,
     MongooseModule.forRoot(
       `mongodb+srv://milan9111work:12345678Aa@cluster0.a6wppdg.mongodb.net/?retryWrites=true&w=majority`,
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
